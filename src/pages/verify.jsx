3d6c9f71dbf931de9bdcc1ad9db7f64e9717262b
채정욱 <jeonguk95@gmail.com>
2025-04-07 18:58:09 +0900

    🔧 verify.scss 경로 수정 및 빌드 에러 해결

--- src/pages/verify.jsx
+++ src/pages/verify.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/global.scss';
-import '../styles/verify.scss';
+import '../styles/Verify.scss';
 
 const Verify = () => {
   const navigate = useNavigate();
